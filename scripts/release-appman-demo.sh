8a571b507b64adc5562198fae20bce92a95d8d9a
Brecht-Pallemans <brecht.pallemans@student.kdg.be>
2020-08-05 13:41:22 +0700

    no message

--- scripts/release-appman-demo.sh
+++ scripts/release-appman-demo.sh
@@ -4,11 +4,11 @@ echo "start tiller"
 export KUBECONFIG=$HOME/.kube/kubeconfig
 helm tiller start-ci
 export HELM_HOST=127.0.0.1:44134
-result=$(eval helm ls | grep helloapp)
+result=$(eval helm ls | grep appman-demo)
 if [ $? -ne "0" ]; then
-   helm install --timeout 180 --name helloapp --set image.tag=$TAG charts/helloapp
+   helm install --timeout 180 --name appman-demo --set image.tag=$TAG charts/appman-demo
 else
-   helm upgrade --timeout 180 helloapp --set image.tag=$TAG charts/helloapp
+   helm upgrade --timeout 180 appman-demo --set image.tag=$TAG charts/appman-demo
 fi
 echo "stop tiller"
 helm tiller stop
